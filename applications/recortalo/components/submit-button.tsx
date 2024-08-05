d3a9003e880edfd2547e5b28e79993f361afa26c
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-05 21:31:36 +0200

    refactor: Add a loading indicator to SubmitButton

--- applications/recortalo/components/submit-button.tsx
+++ applications/recortalo/components/submit-button.tsx
@@ -1,16 +1,18 @@
 "use client"
 
+import { Loader2 } from "lucide-react"
 import type { ComponentProps } from "react"
 import { useFormStatus } from "react-dom"
 import { Button } from "shadcn/button"
 
-export const SubmitButton = ({ children, formState, ...properties }: ComponentProps<"button"> & { formState: any }) => {
+export const SubmitButton = ({ children, formState, ...properties }: ComponentProps<"button"> & { formState?: any }) => {
 	const { pending } = useFormStatus()
 
-	const isSubmitting = formState?.isSubmitting
+	const isSubmitting = pending || formState?.isSubmitting
 
 	return (
-		<Button type="submit" aria-disabled={pending || isSubmitting} disabled={pending || isSubmitting} {...properties}>
+		<Button type="submit" aria-disabled={isSubmitting} disabled={isSubmitting} {...properties}>
+			{isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
 			{children}
 		</Button>
 	)
