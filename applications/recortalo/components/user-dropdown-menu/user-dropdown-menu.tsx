93cbeb608915485844353c9c94dbc64f6b93cd04
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-07 23:53:03 +0200

    refactor: Update user menu

--- applications/recortalo/components/user-dropdown-menu/user-dropdown-menu.tsx
+++ applications/recortalo/components/user-dropdown-menu/user-dropdown-menu.tsx
@@ -1,31 +1,42 @@
-import { getUserSession } from "../../services/get-user-session"
-import { CircleUser } from "lucide-react"
+import { getUserSession } from "services/get-user-session"
 import { Button } from "shadcn/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "shadcn/dropdown-menu"
-import { UserDropdownMenuSignOutForm } from "./user-dropdown-menu-sign-out-form"
+import { UserDropdownMenuSignOutForm } from "components/user-dropdown-menu/user-dropdown-menu-sign-out-form"
 import Link from "next/link"
+import { Avatar, AvatarFallback } from "shadcn/avatar"
 
 export const UserDropdownMenu = async () => {
 	const session = await getUserSession()
-	const metadata = session?.user?.user_metadata
+
+	const userMetadata = session?.user?.user_metadata
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="ghost" size="icon" className="h-8 w-8 px-0 rounded-full">
-					<CircleUser className="h-5 w-5" />
-					<span className="sr-only">Toggle user menu</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuLabel>{metadata?.first_name}</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem className="cursor-pointer">
-					<Link href="/settings">Settings</Link>
-				</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<UserDropdownMenuSignOutForm />
-			</DropdownMenuContent>
-		</DropdownMenu>
+		session && (
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant="ghost" size="icon" className="h-8 w-8 px-0 rounded-full">
+						<Avatar>
+							<AvatarFallback>
+								{userMetadata?.first_name.charAt(0).toUpperCase()}
+								{userMetadata?.last_name?.charAt(0).toUpperCase()}
+							</AvatarFallback>
+						</Avatar>
+						<span className="sr-only">Toggle user menu</span>
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end">
+					<DropdownMenuLabel>
+						{userMetadata?.first_name} {userMetadata?.last_name}
+					</DropdownMenuLabel>
+					<DropdownMenuLabel className="text-sm text-muted-foreground">{session?.user?.email}</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem className="cursor-pointer">
+						<Link href="/settings">Settings</Link>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<UserDropdownMenuSignOutForm />
+				</DropdownMenuContent>
+			</DropdownMenu>
+		)
 	)
 }
