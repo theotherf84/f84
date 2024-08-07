5c4c023c9bc7d81c16525b718cd08a7b8975c71b
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-07 23:51:32 +0200

    fix: Correct layout issues

--- applications/recortalo/app/[locale]/orders/layout.tsx
+++ applications/recortalo/app/[locale]/orders/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({
 }: {
 	children: ReactNode
 }) => (
-	<div className="flex min-h-screen w-full flex-col">
+	<div className="flex min-h-screen w-full flex-col bg-muted/40">
 		<Header />
 		<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">{children}</main>
 	</div>
--- applications/recortalo/app/[locale]/orders/page.tsx
+++ applications/recortalo/app/[locale]/orders/page.tsx
@@ -1,13 +1,11 @@
 import { OrdersActionCard } from "components/orders-action-card"
 import { OrdersTable } from "components/orders-table/orders-table"
 
-const Page = async () => (
-	<div className="grid grid-cols-3 col-span-3 auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3 bg-muted/40">
-		<div className="col-span-2">
-			<div className="flex flex-col gap-6">
-				<OrdersActionCard />
-				<OrdersTable />
-			</div>
+const Page = () => (
+	<div className="grid grid-cols-3 col-span-3 auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3">
+		<div className="grid col-span-3 md:col-span-2 gap-8">
+			<OrdersActionCard />
+			<OrdersTable />
 		</div>
 	</div>
 )
