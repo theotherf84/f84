9089a852175c02ad2d0a46aebc2fb75ea190b2ae
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-10 16:56:35 +0200

    chore: Replace relative import statement

--- applications/recortalo/components/employees-table/employees-table-columns.tsx
+++ applications/recortalo/components/employees-table/employees-table-columns.tsx
@@ -1,10 +1,10 @@
-import { TableHead, TableHeader, TableRow } from "../../shadcn/table"
+import { TableHead, TableHeader, TableRow } from "shadcn/table"
 
 export const EmployeesTableHeader = () => (
 	<TableHeader>
 		<TableRow>
-		<TableHead>Avatar</TableHead>
-		<TableHead>First name</TableHead>
+			<TableHead>Avatar</TableHead>
+			<TableHead>First name</TableHead>
 			<TableHead>Last name</TableHead>
 			<TableHead className="hidden sm:table-cell">Created at</TableHead>
 		</TableRow>
