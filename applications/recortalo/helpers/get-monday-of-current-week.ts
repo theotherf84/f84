4855ad68defd7c2df78f180fae80acc43d509b81
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-12 16:27:25 +0200

    feat: Add helper functions to handle date operations

---
+++ applications/recortalo/helpers/get-monday-of-current-week.ts
@@ -0,0 +1,15 @@
+export const getMondayOfCurrentWeek = (date?: Date) => {
+	const currentDate = date || new Date()
+
+	const day = currentDate?.getDay()
+
+	// Calculates the difference between the current date and the day of the week.
+	// If the day is Sunday, starting as 0, the difference is -6, since Monday is 6 days ahead of Sunday.
+	// Otherwise, the difference is 1, since Monday is 1 day ahead of the current day.
+	const difference = currentDate?.getDate() - day + (day === 0 ? -6 : 1)
+
+	currentDate.setDate(difference)
+	currentDate.setUTCHours(0, 0, 0, 0)
+
+	return currentDate.toISOString()
+}
