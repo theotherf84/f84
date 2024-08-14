c92a42086d494236ff8713c4f6fd6910d6aab40a
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-15 01:01:46 +0200

    feat: Expand the weekly revenue card component

--- applications/recortalo/app/[locale]/page.tsx
+++ applications/recortalo/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
-import { DailyRevenueCardContainer } from "components/daily-revenue-card/daily-revenue-card-container"
 import Header from "components/header"
+import { WeeklyRevenueCardContainer } from "components/weekly-revenue-card/weekly-revenue-card-container"
 import { getTranslations } from "helpers/translations"
 
 const Page = async ({
@@ -13,7 +13,7 @@ const Page = async ({
 		<>
 			<Header />
 			<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8 lg:grid-cols-2 xl:grid-cols-2 py-6">
-				<DailyRevenueCardContainer />
+				<WeeklyRevenueCardContainer />
 			</main>
 		</>
 	)
--- applications/recortalo/components/daily-revenue-card/daily-revenue-card-container.tsx
+++
@@ -1,5 +0,0 @@
-import { DailyRevenueCard } from "components/daily-revenue-card/daily-revenue-card"
-
-export const DailyRevenueCardContainer = () => {
-	return <DailyRevenueCard />
-}
