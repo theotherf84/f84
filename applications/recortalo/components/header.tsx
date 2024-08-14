a0f844703b2da6015068ad9b7c880fd2fa5efe39
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-14 23:37:37 +0200

    chore: Update header styling

--- applications/recortalo/components/header.tsx
+++ applications/recortalo/components/header.tsx
@@ -18,7 +18,7 @@ const navigationItems = [
 ]
 
 const Header = () => (
-	<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+	<header className="flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
 		<nav className="hidden flex-col text-lg font-medium md:flex md:flex-row md:items-center justify-between md:gap-5 md:text-sm lg:gap-6">
 			<Link href="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
 				<Scissors className="h-6 w-6" />
