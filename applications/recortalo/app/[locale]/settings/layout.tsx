7cf4c97a4a9059b5f854f2b8b5b641a5cb0c1d62
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-15 16:11:52 +0200

    fix: Correct import statement type

--- applications/recortalo/app/[locale]/settings/layout.tsx
+++ applications/recortalo/app/[locale]/settings/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "components/header"
 import { SettingsNavigationMenu } from "components/settings-navigation-menu"
-import * as React from "react"
+import type * as React from "react"
 
 const Layout = ({
 	children,
