7f32dc0f6eebb6dac4a1dde59b727d042e051a38
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-23 21:11:33 +0200

    feat: Add phone number input

--- types/components.ts
+++ types/components.ts
@@ -1,4 +1,11 @@
-import type { Dispatch, InputHTMLAttributes, SetStateAction, TextareaHTMLAttributes } from "react"
+import type { DialogProps } from "@radix-ui/react-dialog"
+import type { countries } from "constants/countries"
+import type { CarrierCode, CountryCallingCode, CountryCode, E164Number, NationalNumber, NumberType } from "libphonenumber-js"
+import type { ComponentPropsWithoutRef, Dispatch, InputHTMLAttributes, SetStateAction, TextareaHTMLAttributes } from "react"
+
+export type CommandDialogProperties = DialogProps
+
+export type Country = (typeof countries)[number]
 
 export interface DatePickerProperties {
 	date: Date
@@ -7,5 +14,24 @@ export interface DatePickerProperties {
 }
 
 export type InputProperties = InputHTMLAttributes<HTMLInputElement>
+
+export type PhoneData = {
+	phoneNumber?: E164Number
+	countryCode?: CountryCode
+	countryCallingCode?: CountryCallingCode
+	carrierCode?: CarrierCode
+	nationalNumber?: NationalNumber
+	internationalNumber?: string
+	possibleCountries?: string
+	isValid?: boolean
+	isPossible?: boolean
+	uri?: string
+	type?: NumberType
+}
+
+export interface PhoneInputProperties extends ComponentPropsWithoutRef<"input"> {
+	value?: string
+	defaultCountry?: CountryCode
+}
 
 export type TextareaProperties = TextareaHTMLAttributes<HTMLTextAreaElement>
