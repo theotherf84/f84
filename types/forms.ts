f71fa784fc06c4f162595b2801619cb859b8f891
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-08-25 20:36:29 +0200

    chore: Create type support for the add employee form

--- types/forms.ts
+++ types/forms.ts
@@ -1,14 +1,23 @@
-import type { formSchema } from "components/(orders)/orders-quick-add-form/orders-quick-add-form-schema"
+import type { formSchema as addEmployeeFormSchema } from "components/(employees)/employees-add-form/employees-add-form-schema"
+import type { formSchema as quickAddOrderFormSchema } from "components/(orders)/orders-quick-add-form/orders-quick-add-form-schema"
 import type { Category, Employee, Subcategory } from "types/tables.types"
 import type { z as zod } from "zod"
 
-export interface QuickAddOrderFormProperties {
+export interface FormProperties {
+	onSubmit: () => void
+}
+
+export type AddEmployeeFormFieldValues = zod.infer<typeof addEmployeeFormSchema>
+
+// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
+export interface AddEmployeeFormProperties {}
+
+export interface QuickAddOrderFormProperties extends FormProperties {
 	categories: Category[]
 	employees: Employee[]
 	subcategories: Subcategory[]
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	translations?: Record<string, any>
-	onSubmit: () => void
 }
 
 export interface QuickAddOrderFormSheetProperties {
@@ -19,4 +28,4 @@ export interface QuickAddOrderFormSheetProperties {
 	translations?: Record<string, any>
 }
 
-export type QuickAddOrderFormFieldValues = zod.infer<typeof formSchema>
+export type QuickAddOrderFormFieldValues = zod.infer<typeof quickAddOrderFormSchema>
