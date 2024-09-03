f2a22d435d2bf2b88356c0568b0a3c9f5a186512
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-09-03 19:47:01 +0200

    chore: Enable free number input in the quick add order form

--- components/(orders)/orders-quick-add-form/orders-quick-add-form-stepper.tsx
+++ components/(orders)/orders-quick-add-form/orders-quick-add-form-stepper.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import type { formSchema } from "components/(orders)/orders-quick-add-form/orders-quick-add-form-schema"
+import { mergeClassNames } from "helpers/merge-class-names"
 import { Minus, Plus } from "lucide-react"
 import type { Control } from "react-hook-form"
 import { Button } from "shadcn/button"
-import { FormControl, FormField, FormItem } from "shadcn/form"
+import { FormControl, FormField, FormItem, FormLabel } from "shadcn/form"
 import { Input } from "shadcn/input"
-import type { z as zod } from "zod"
+import { Label } from "shadcn/label"
+import type * as zod from "zod"
 
 export const OrdersQuickAddFormStepper = ({ control }: { control: Control<zod.infer<typeof formSchema>> }) => {
 	const handleOnDecreaseCost = (field) => field.onChange(field.value - 500)
@@ -19,7 +21,10 @@ export const OrdersQuickAddFormStepper = ({ control }: { control: Control<zod.in
 			name="cost"
 			defaultValue={6000}
 			render={({ field }) => (
-				<>
+				<FormItem>
+					<FormLabel asChild>
+						<Label htmlFor="first-name">Costo</Label>
+					</FormLabel>
 					<div className="flex items-center justify-center">
 						<Button
 							variant="outline"
@@ -35,10 +40,17 @@ export const OrdersQuickAddFormStepper = ({ control }: { control: Control<zod.in
 							<Minus className="h-4 w-4" />
 							<span className="sr-only">Decrease</span>
 						</Button>
-						<div className="flex flex-1 flex-col gap-2 my-4 text-center">
-							<div className="text-6xl font-bold tracking-tighter">{field.value}</div>
-							<div className="text-xs uppercase text-muted-foreground">Costo del servicio</div>
-						</div>
+						<FormControl className="text-center">
+							<Input
+								className={mergeClassNames(
+									"h-full text-6xl font-bold tracking-tighter border-none outline-none ring-0 focus:ring-0 focus:outline-none focus-visible:ring-transparent focus-visible:outline-none focus-visible:shadow-none",
+									"[-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none",
+								)}
+								type="number"
+								{...field}
+								onChange={(event) => field.onChange(+event.target.value)}
+							/>
+						</FormControl>
 						<Button
 							variant="outline"
 							size="icon"
@@ -54,12 +66,7 @@ export const OrdersQuickAddFormStepper = ({ control }: { control: Control<zod.in
 							<span className="sr-only">Increase</span>
 						</Button>
 					</div>
-					<FormItem hidden>
-						<FormControl>
-							<Input type="number" {...field} onChange={(event) => field.onChange(+event.target.value)} />
-						</FormControl>
-					</FormItem>
-				</>
+				</FormItem>
 			)}
 		/>
 	)
