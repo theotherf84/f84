353406f99f617f343d484240c9e6b356de9fceb3
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-09-13 00:41:49 +0200

    fix: Update translation references

--- components/(orders)/orders-action-card.tsx
+++ components/(orders)/orders-action-card.tsx
@@ -1,23 +1,21 @@
 import { QuickAddOrderFormContainer } from "components/(orders)/orders-quick-add-form/orders-quick-add-form-container"
-import { getTranslations } from "helpers/translations"
+import { getTranslations } from "helpers/get-translations"
 import Link from "next/link"
 import { Button } from "shadcn/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "shadcn/card"
 
 export const OrdersActionCard = async () => {
-	const translations = await getTranslations()
-
-	const { action, headline, title } = translations?.orders?.pages?.root?.actionCard
+	const { ordersPageActionCardMainButtonLabel, ordersPageActionCardSubtitle, ordersPageActionCardTitle } = await getTranslations()
 
 	return (
 		<Card className="flex flex-1 flex-col gap-4 justify-between">
 			<CardHeader>
-				<CardTitle>{title}</CardTitle>
-				<CardDescription className="leading-relaxed">{headline}</CardDescription>
+				<CardTitle>{ordersPageActionCardTitle}</CardTitle>
+				<CardDescription className="leading-relaxed">{ordersPageActionCardSubtitle}</CardDescription>
 			</CardHeader>
 			<CardContent className="flex flex-row gap-2">
 				<Button disabled>
-					<Link href="/orders/add">{action}</Link>
+					<Link href="/orders/add">{ordersPageActionCardMainButtonLabel}</Link>
 				</Button>
 				<QuickAddOrderFormContainer />
 			</CardContent>
--- components/(orders)/orders-quick-add-form/orders-quick-add-form-container.tsx
+++ components/(orders)/orders-quick-add-form/orders-quick-add-form-container.tsx
@@ -1,26 +1,23 @@
 import { QuickAddOrderForm } from "components/(orders)/orders-quick-add-form/orders-quick-add-form"
 import { ContentSheet } from "components/content-sheet"
-import { getTranslations } from "helpers/translations"
+import { getTranslations } from "helpers/get-translations"
 import { getCategories } from "services/get-categories"
 import { getEmployees } from "services/get-employees"
 import { getSubcategories } from "services/get-subcategories"
 
 export const QuickAddOrderFormContainer = async () => {
-	const { orders } = await getTranslations()
-
-	const { quickAction } = orders?.pages?.root?.actionCard
-	const { headline, title } = orders?.forms?.quickAdd
+	const { ordersPageActionCardQuickButtonLabel, quickAddOrderFormSheetSubtitle, quickAddOrderFormSheetTitle } = await getTranslations()
 
 	const [categories, employees, subcategories] = await Promise.all([getCategories(), getEmployees(), getSubcategories()])
 
+	const messages = {
+		action: ordersPageActionCardQuickButtonLabel,
+		subtitle: quickAddOrderFormSheetSubtitle,
+		title: quickAddOrderFormSheetTitle,
+	}
+
 	return (
-		<ContentSheet
-			messages={{
-				action: quickAction,
-				subtitle: headline,
-				title,
-			}}
-		>
+		<ContentSheet messages={messages}>
 			<QuickAddOrderForm categories={categories} employees={employees} subcategories={subcategories} />
 		</ContentSheet>
 	)
