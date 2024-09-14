3d03f92fc87cc5a578a729b78e9d3f1e17d5b843
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-09-14 20:32:02 +0200

    feat: Add translations to the update password form

--- components/(authentication)/update-password-forms/update-password-form.tsx
+++ components/(authentication)/update-password-forms/update-password-form.tsx
@@ -1,32 +1,35 @@
 import { action } from "actions/update-password"
 import { SubmitButton } from "components/submit-button"
+import { getTranslations } from "helpers/get-translations"
 import { Siren } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "shadcn/alert"
 import { Input } from "shadcn/input"
 import { Label } from "shadcn/label"
 
-export const UpdatePasswordForm = ({ error }: { error: string }) => {
+export const UpdatePasswordForm = async ({ error }: { error: string }) => {
+	const translations = await getTranslations()
+
 	return (
 		<div className="flex h-full items-center justify-center">
 			<div className="mx-auto grid w-1/2 gap-6">
 				<div className="grid gap-2 text-left">
-					<h1 className="text-3xl font-bold">Update password</h1>
-					<p className="text-muted-foreground">Enter your new password</p>
+					<h1 className="text-3xl font-bold">{translations.updatePasswordFormTitle}</h1>
+					<p className="text-muted-foreground">{translations.updatePasswordFormSubtitle}</p>
 				</div>
 				<form action={action} className="grid gap-4">
 					<div className="grid gap-2">
 						<div className="flex items-center">
-							<Label htmlFor="password">Password</Label>
+							<Label htmlFor="password">{translations.updatePasswordFormPasswordInputLabel}</Label>
 						</div>
 						<Input id="password" name="password" type="password" required />
 					</div>
 					<div className="grid gap-2">
 						<div className="flex items-center">
-							<Label htmlFor="password-confirmation">Password confirmation</Label>
+							<Label htmlFor="password-confirmation">{translations.updatePasswordFormConfirmPasswordInputLabel}</Label>
 						</div>
 						<Input id="password-confirmation" name="password-confirmation" type="password" required />
 					</div>
-					<SubmitButton>Update password</SubmitButton>
+					<SubmitButton>{translations.updatePasswordFormSubmitButtonLabel}</SubmitButton>
 					{error && (
 						<Alert>
 							<Siren className="h-4 w-4" />
