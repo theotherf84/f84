a02882bc9b26a14cec77a9a00c2f454b331d3792
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-09-16 23:10:16 +0200

    chore: Update color theme toggle component

--- components/color-theme-toggle.tsx
+++ components/color-theme-toggle.tsx
@@ -5,8 +5,9 @@ import { useTheme } from "next-themes"
 import React from "react"
 import { Button } from "shadcn/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "shadcn/dropdown-menu"
+import type { ColorThemeToggleProperties } from "types/components"
 
-const ColorThemeToggle = () => {
+export const ColorThemeToggle = ({ messages }: ColorThemeToggleProperties) => {
 	const { setTheme } = useTheme()
 
 	return (
@@ -15,16 +16,14 @@ const ColorThemeToggle = () => {
 				<Button variant="ghost" size="icon" className="h-8 w-8 px-0 rounded-full">
 					<SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle color theme</span>
+					<span className="sr-only">{messages.accessibility.screenReader.toggleColorTheme}</span>
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("light")}>{messages.colorTheme.light}</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("dark")}>{messages.colorTheme.dark}</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("system")}>{messages.colorTheme.system}</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
 }
-
-export { ColorThemeToggle }
