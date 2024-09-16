5f99ff822e279cc8d9338e000d3856f8b9f47447
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-09-16 23:11:57 +0200

    feat: Update orders layout and page

--- app/[locale]/orders/layout.tsx
+++ app/[locale]/orders/layout.tsx
@@ -1,11 +1,7 @@
 import Header from "components/header"
 import type { ReactNode } from "react"
 
-const Layout = ({
-	children,
-}: {
-	children: ReactNode
-}) => (
+const Layout = ({ children }: { children: ReactNode }) => (
 	<>
 		<Header />
 		<main className="grid gap-6 h-screen">{children}</main>
--- app/[locale]/orders/page.tsx
+++ app/[locale]/orders/page.tsx
@@ -1,26 +1,35 @@
 import { OrdersDataTableContainer } from "components/(orders)/orders-data-table/orders-data-table-container"
 import { QuickAddOrderFormContainer } from "components/(orders)/orders-quick-add-form/orders-quick-add-form-container"
-import { ActionCard } from "components/action-card"
+import Link from "next/link"
+import { Button } from "shadcn/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "shadcn/card"
 
 const Page = () => (
 	<div className="md:container md:mx-auto p-6">
 		<div className="grid col-span-3 md:col-span-2 gap-6">
 			<div className="flex flex-col md:flex-row gap-4">
-				<ActionCard
-					disabled
-					messages={{
-						action: "Crear nueva venta",
-						subtitle: "Presentamos nuestro panel de ventas dinámico para una gestión perfecta y un análisis detallado.",
-						title: "Tus ventas",
-					}}
-					pathname="/orders/add"
-				>
-					<QuickAddOrderFormContainer />
-				</ActionCard>
-				<ActionCard
-					messages={{ action: "Ver reporte", subtitle: "Un reporte diario de ventas por empleado.", title: "Reporte del dia" }}
-					pathname="/orders/employees"
-				/>
+				<Card className="flex flex-1 flex-col gap-4 justify-between">
+					<CardHeader>
+						<CardTitle>Tus ventas</CardTitle>
+						<CardDescription className="leading-relaxed">
+							Presentamos nuestro panel de ventas dinámico para una gestión perfecta y un análisis detallado
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="flex flex-row gap-2">
+						<QuickAddOrderFormContainer />
+					</CardContent>
+				</Card>
+				<Card className="flex flex-1 flex-col gap-4 justify-between">
+					<CardHeader>
+						<CardTitle>Reporte del dia</CardTitle>
+						<CardDescription className="leading-relaxed">Un reporte diario de ventas por empleado</CardDescription>
+					</CardHeader>
+					<CardContent className="flex flex-row gap-2">
+						<Button>
+							<Link href="/orders/employees">Ver reporte</Link>
+						</Button>
+					</CardContent>
+				</Card>
 			</div>
 			<OrdersDataTableContainer />
 		</div>
